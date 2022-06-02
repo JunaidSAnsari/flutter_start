ed3e6d86495caeaed3b76d1e49fd19be0d68bac9
elib-mtn-01 <thejunaidsaleem@gmail.com>
2022-06-02 14:05:13 +0500

    DropDownMenu completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: FAB(),));
+void main() {
+  runApp(DDMenu());
 }
 
-class FAB extends StatelessWidget {
-  const FAB({Key? key}) : super(key: key);
+class DDMenu extends StatefulWidget {
+  const DDMenu({Key? key}) : super(key: key);
+
+  @override
+  State<DDMenu> createState() => _DDMenuState();
+}
+
+class _DDMenuState extends State<DDMenu> {
+  List<String> list = ["Select Country", "Pakistan", "India", "Bangladesh"];
+  String selectedCountry = "Select Country";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Floating Action Button"),),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Icon(Icons.message),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Text("Floating action button"),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Select country from drop down menu"),
+        ),
+        body: Center(
+          child: DropdownButton(
+            value: selectedCountry,
+            onChanged: (String? newCountry) {
+              setState(() {
+                selectedCountry = newCountry!;
+              });
+            },
+            items: list.map<DropdownMenuItem<String>>((String v) {
+              return DropdownMenuItem(
+                value: v,
+                child: Text(v),
+              );
+            }).toList(),
+          ),
+        ),
       ),
     );
   }
