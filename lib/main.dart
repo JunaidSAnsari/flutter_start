83bbe3ecd09c5ba803789792ee4476b7deae4ba9
JunaidSAnsari <thejunaidsaleem@gmail.com>
2022-06-14 15:10:55 +0500

    Toggle Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,38 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
-      child: Text(
-        "App Now",
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            color: Colors.red,
-            fontSize: 40,
-            fontStyle: FontStyle.italic,
-            backgroundColor: Colors.amber),
-      ),
-    ),
-  );
+void main(){
+  runApp(MaterialApp(
+    home: toggleButton(),
+  ));
+}
+class toggleButton extends StatefulWidget {
+  const toggleButton({Key? key}) : super(key: key);
+
+  @override
+  State<toggleButton> createState() => _toggleButtonState();
 }
 
-// //MaterialApp, Material
-// void main() {
-//   runApp(MaterialApp(
-//     home: Material(
-//       child: Center(
-//         child: Text(
-//           "App Now",
-//           textDirection: TextDirection.ltr,
-//           style: TextStyle(
-//               color: Colors.red, fontSize: 40, fontStyle: FontStyle.italic),
-//         ),
-//       ),
-//     ),
-//   ));
-// }
+class _toggleButtonState extends State<toggleButton> {
+  List<bool> list = [true, false, false];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Toggle Button Design"),),
+      body: Center(
+        child: ToggleButtons(
+          children: <Widget>[
+            Icon(Icons.ac_unit),
+            Icon(Icons.call),
+            Icon(Icons.cake),
+          ],
+          onPressed: (int index) {
+            setState(() {
+              list[index] = !list[index];
+            });
+          },
+          isSelected: list,
+        ),
+      ),
+    );
+  }
+}
